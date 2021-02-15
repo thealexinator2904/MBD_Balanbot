f2ec75d764e73f483e7fb6b242323a5a2b01771f
Alexander Grebien <59167072+thealexinator2904@users.noreply.github.com>
2021-02-15 12:14:10 +0100

    Tested non linearized model

--- Matlab_stuff/variablen_ersteller.m
+++ Matlab_stuff/variablen_ersteller.m
@@ -2,10 +2,46 @@ clc
 clear all
 format short eng
 format compact
+close all
 
 m = 0.1756;
 M = 0.7160;
 g = 9.810;
 J = 0.001;
 l = 0.11;
-mue = 0.1;
+b = 0.1;
+
+%simulate with theta = 0 degrees
+F=0;
+Theta_0=0;
+struct_res = sim('master', 60);
+hgexport(plot_non_linear(struct_res),'..\Lab_report\pics\plots\non_linearized_results_theta_0.eps');
+
+%simulate with theta = 5 degrees
+F=0;
+Theta_0=5;
+struct_res = sim('master', 60);
+hgexport(plot_non_linear(struct_res),'..\Lab_report\pics\plots\non_linearized_results_theta_5.eps');
+
+%simulate with theta = 5 degrees and increased friction
+F=0;
+Theta_0=5;
+b=0.6;
+struct_res = sim('master', 60);
+hgexport(plot_non_linear(struct_res),'..\Lab_report\pics\plots\non_linearized_results_inc_friction.eps');
+b=0.1;
+
+function [fig] = plot_non_linear(struct)
+    fig = figure('Name', 'non Linear Model simulation results');
+    subplot(2,1,1)
+    plot(struct.logsout{1}.Values.Time, struct.logsout{1}.Values.Data);
+    xlabel('time in s')
+    ylabel('theta in rad')
+    grid on
+    
+    subplot(2,1,2)
+    plot(struct.logsout{2}.Values.Time, struct.logsout{2}.Values.Data);
+    ylabel('x in m')
+    xlabel('time in s')
+    grid on
+end
